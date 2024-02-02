fb2bfa6bac369506fc73d3a952b4b1d8f1484638
ryanjzheng <rzheng25@terpmail.umd.edu>
2024-02-02 18:04:49 -0500

    testing firebase connection

---
+++ pnaa/src/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAef0b2KDrdWCcKJBmOW88PX4FZLtrn8Co",
+  authDomain: "pnaa-8b56f.firebaseapp.com",
+  projectId: "pnaa-8b56f",
+  storageBucket: "pnaa-8b56f.appspot.com",
+  messagingSenderId: "1072615861967",
+  appId: "1:1072615861967:web:8ed5ad49a18b5a2b0651ba",
+  measurementId: "G-HW8Z1LHJSM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;

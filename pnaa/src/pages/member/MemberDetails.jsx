7d9de9b2d4b6274c51b44a9004992c4734edac7c
Emily Li <emilyli@wireless-10-104-139-123.umd.edu>
2024-02-24 17:37:29 -0500

    right side front end

--- pnaa/src/pages/member/MemberDetails.jsx
+++ pnaa/src/pages/member/MemberDetails.jsx
@@ -43,46 +43,124 @@ const MemberDetail = () => {
 
         <div className={styles["member-detail-information-container"]}>
           <div className={styles["member-detail-information-container-left"]}>
-          <table className={styles["member-detail-table"]}>
-                <tr>
-                    <td><p className={styles["membership-card-label-header"]}>MEMBERSHIP CARD</p></td>
-                    <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td><div className={styles["membership-card"]}>
-                    <p>
-                      Name: {member.FirstName} {member.LastName}
-                    </p>
-                    <p>ID: {member.id}</p>
-                    {/* ... Example Fields ... */}
-                  </div></td>
-                </tr>
-                <tr>
-                  <td><p className={styles["membership-card-label-header"]}>MEMBERSHIP STATUS</p></td>
-                  <td><p className={styles["membership-card-label-data"]}>Active</p></td>
-                </tr>
-                <tr>
-                  <td><p className={styles["membership-card-label-header"]}>REGISTRATION</p></td>
-                  <td><p className={styles["membership-card-label-data"]}>01.01.0001</p></td>
-                </tr>
-                <tr>
-                  <td><p className={styles["membership-card-label-header"]}>RENEWAL</p></td>
-                  <td><p className={styles["membership-card-label-data"]}>01.01.0001</p></td>
-                </tr>
-                <tr>
-                  <td><p className={styles["membership-card-label-header"]}>RENEWAL DUE</p></td>
-                  <td><p className={styles["membership-card-label-data"]}>01.01.0001</p></td>
-                </tr>
-                <tr>
-                  <td><p className={styles["membership-card-label-header"]}>LEVEL LAST UPDATED</p></td>
-                  <td><p className={styles["membership-card-label-data"]}>01.01.0001</p></td>
-                </tr>
-            </table>
+          <table className={styles["membership-card-detail-table"]}>
+            <tr>
+              <td><p className={styles["membership-label"]}>MEMBERSHIP CARD</p></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><div className={styles["membership-card"]}>
+                <p>
+                  Name: {member.FirstName} {member.LastName}
+                </p>
+                <p>ID: {member.id}</p>
+                {/* ... Example Fields ... */}
+              </div></td>
+            </tr>
+            <tr>
+              <td><p className={styles["membership-label"]}>MEMBERSHIP STATUS</p></td>
+              <td><p className={styles["membership-card-label-data"]}>Active</p></td>
+            </tr>
+            <tr>
+              <td><p className={styles["membership-label"]}>REGISTRATION</p></td>
+              <td><p className={styles["membership-card-label-data"]}>01.01.0001</p></td>
+            </tr>
+            <tr>
+              <td><p className={styles["membership-label"]}>RENEWAL</p></td>
+              <td><p className={styles["membership-card-label-data"]}>01.01.0001</p></td>
+            </tr>
+            <tr>
+              <td><p className={styles["membership-label"]}>RENEWAL DUE</p></td>
+              <td><p className={styles["membership-card-label-data"]}>01.01.0001</p></td>
+            </tr>
+            <tr>
+              <td><p className={styles["membership-label"]}>LEVEL LAST UPDATED</p></td>
+              <td><p className={styles["membership-card-label-data"]}>01.01.0001</p></td>
+            </tr>
+          </table>
         
           </div>  
           <div className={styles["member-detail-information-container-right"]}>
-            <h1>UGHHHH</h1>
+            <table className={styles["member-detail-table"]}>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>GENDER</p></td>
+                <td>UHHHH {member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>AGE</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>DONATED/SERVED HOURS</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['underline']}`}>NURSING EDUCATION</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>SCHOOL NAME</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>YEAR GRADUATED</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>COUNTRY</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>DEGREE RECEIVED</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>HIGHEST LEVEL OF EDUCATION</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['underline']}`}>WORK INFORMATION</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>STATUS</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>EMPLOYER</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>TYPE OF INSTITUTION</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>PRIMARY NURSING PRACTICE</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>ADVANCED PRACTICE NURSES</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>YEAR OF NURSING EXPERIENCES</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>NATIONAL CERTIFICATION</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['small']}`}>YEARS AS PNAA CHAPTER MEMBER</p></td>
+                <td>{member.gender}</td>
+              </tr>
+              <tr>
+                <td><p className={`${styles['membership-label']} ${styles['underline']}`}>NOTES</p></td>
+                <td></td>
+              </tr>
+            </table>
+            <p className={styles["member-notes"]}>{member.notes}</p>
           </div>
         </div>
       </div>

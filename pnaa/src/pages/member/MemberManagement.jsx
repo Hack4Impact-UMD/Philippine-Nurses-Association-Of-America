b630ce51628db199f9892cf6371a9a5b97a7e40f
Aaryan <apat9@terpmail.umd.edu>
2024-02-26 02:55:09 -0500

    Added components for status icon and buttons

--- pnaa/src/pages/member/MemberManagement.jsx
+++ pnaa/src/pages/member/MemberManagement.jsx
@@ -38,11 +38,67 @@ const MemberManagement = () => {
     return <div>Loading...</div>;
   }
 
+  //Generic component definition to create the icons for the "status" column
+  const Status = ({ text, backgroundColor, textColor, width, height }) => {
+    const styles = {
+      status: {
+        backgroundColor: backgroundColor,
+        color: textColor,
+        width: width,
+        height: height,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '10px',
+      },
+    };
+
+    return (
+      <div style={styles.status}>
+        {text}
+      </div>
+    );
+  }
+
+  //Generic component definition to create the buttons for add, suspend, and renew member (need to add onClick functionality later)
+  const MemberButton = ({ text, backgroundColor, width, height }) => {
+    const styles = {
+      memberButton: {
+        backgroundColor: backgroundColor,
+        color: 'white',
+        width: width,
+        height: height,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '6px',
+      },
+    };
+
+    return (
+      <div style={styles.memberButton}>
+        {text}
+      </div>
+    );
+  }
+
+  //Button for "Add Member" created using MemberButton
+  const AddMember = (
+    <div style={{ marginRight: '10px'}}>
+      <MemberButton
+        text="+ Add Member"
+        backgroundColor={"#05208B"}
+        width="139px"
+        height="32px"
+      />
+    </div>
+  );
+
   const columns = [
     { field: '#', headerName: '#', width: 75 },
     { field: 'name', headerName: 'NAME', width: 275, valueGetter: (params) => params.row.id },
-    { field: 'membership-level', headerName: 'MEMBERSHIP LEVEL', width: 250},
-    { field: 'status', headerName: 'STATUS', width: 150},
+    { field: 'membership-level', headerName: 'MEMBERSHIP LEVEL', width: 250, renderCell: (params) => ( <Status text="Active Member (1 year)" backgroundColor={"#EBF0FA"} textColor="blue" width="163px" height="20px"/> ) },
+    { field: 'status', headerName: 'STATUS', width: 150, renderCell: (params) => ( <Status text="Active" backgroundColor={"#E1FCEF"} textColor="green" width="58px" height="20px"/> ) },
     { field: 'registration', headerName: 'REGISTRATION', width: 150},
     { field: 'renewal', headerName: 'RENEWAL', width: 150},
     { field: 'renewal-due', headerName: 'RENEWAL DUE', width: 150},
@@ -53,6 +109,9 @@ const MemberManagement = () => {
     <div>
       <h1>Member Management Page</h1>
       <div style={{ height: '80%', width: '100%' }}>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px' }}>
+          {AddMember}
+        </div>
         <DataGrid
           rows={members}
           columns={columns}
@@ -60,6 +119,7 @@ const MemberManagement = () => {
           rowsPerPageOptions={[5, 10, 20]}
           checkboxSelection
           columnHeaderHeight={100}
+          headerRight={AddMember}
           sx={{
             border: 10,
             borderColor: '#E0E0E0',

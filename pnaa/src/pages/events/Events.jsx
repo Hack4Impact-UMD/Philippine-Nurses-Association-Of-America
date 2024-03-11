b405d916c3e66703a7d44a81fc282cb829f1e186
Aaryan <apat9@terpmail.umd.edu>
2024-03-11 19:16:14 -0400

    Added functions to create components

--- pnaa/src/pages/events/Events.jsx
+++ pnaa/src/pages/events/Events.jsx
@@ -1,6 +1,48 @@
 import React from 'react';
 import { useUser } from '../../config/UserContext';
 
+const Status = ({ text, backgroundColor, textColor, width, height }) => {
+  const styles = {
+    status: {
+      backgroundColor: backgroundColor,
+      color: textColor,
+      width: width,
+      height: height,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: '10px',
+    },
+  };
+
+  return (
+    <div style={styles.status}>
+      {text}
+    </div>
+  );
+}
+
+const MemberButton = ({ text, backgroundColor, width, height }) => {
+  const styles = {
+    memberButton: {
+      backgroundColor: backgroundColor,
+      color: 'white',
+      width: width,
+      height: height,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: '6px',
+    },
+  };
+
+  return (
+    <div style={styles.memberButton}>
+      {text}
+    </div>
+  );
+}
+
 const Events = () => {
 
   const { currentUser, loading } = useUser();
@@ -16,5 +58,7 @@ const Events = () => {
     </div>
   );
 };
+
+
 
 export default Events;

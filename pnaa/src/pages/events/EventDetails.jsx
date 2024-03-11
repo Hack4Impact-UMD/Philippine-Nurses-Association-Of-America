ecc00a87c9c35c42a841e8c3c80897f5bb7a03a5
Emily Li <emilyli@wireless-10-104-139-123.umd.edu>
2024-03-11 18:27:05 -0400

    made new branch

---
+++ pnaa/src/pages/events/EventDetails.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import styles from "./EventDetails.module.css";
+
+
+const EventDetail = () => {
+  
+};
+
+export default EventDetail;

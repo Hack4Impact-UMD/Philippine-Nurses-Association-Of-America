3b01ff7f14349f275e1419407f3c11bbe28afd5f
Aaryan <apat9@terpmail.umd.edu>
2024-04-02 00:49:04 -0400

    Sign in page complete

--- pnaa/src/auth/SignIn.jsx
+++ pnaa/src/auth/SignIn.jsx
@@ -5,6 +5,7 @@ import { auth } from "../config/firebase";
 import { signInWithGoogle } from "./AuthService";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 import "./SignIn.css"
+import PNAA_Logo from "../pages/member/PNAA_Logo.png"; 
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -60,6 +61,8 @@ const SignIn = () => {
 
   return (
     <div id="background">
+      <p id="orgname">Philippine Nurses Association of America</p>
+      <p id="mantra"><span style={{color:'#0533F3'}}>Shine</span><span style={{color:'#AB2218'}}> PNAA </span><span style={{color:'#F4D44C'}}>Shine</span></p>
       <div id="container">
       <h2 id="login">Login</h2>
       <input
@@ -69,6 +72,7 @@ const SignIn = () => {
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Member ID / Email Address"
       />
+      <p id="forgot">Forgot Password?</p>
       <input
         id="password"
         type={showPassword ? "text" : "password"}
@@ -76,7 +80,7 @@ const SignIn = () => {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
-      <label htmlFor="showPassword">
+      <label id="passwordLabel" htmlFor="showPassword">
           <input
             id="showPassword"
             type="checkbox"
@@ -87,11 +91,12 @@ const SignIn = () => {
       </label>
       <button id="loginbutton" onClick={handleSignIn}>Login</button>
       {/* <button onClick={handleGoogleSignIn}>Sign in with Google</button> */}
-      {error && <p style={{ color: "red" }}>{error}</p>}{" "}
+      {error && <p id="error" style={{ color: "red" }}>{error}</p>}{" "}
       <p id="dont">
         Don't have an account? <Link to="/signup">Sign Up</Link>
       </p>
       </div>
+      <img src={PNAA_Logo} alt="PNAA Logo" id="logo"/>
     </div>
   );
 };

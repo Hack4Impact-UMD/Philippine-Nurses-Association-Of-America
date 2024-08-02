067c78a4d77b392cfb6194b9f7f9a37e68188d75
sophtsai <sophietsai31@gmail.com>
2024-08-01 22:16:16 -0400

    Complete chapter selection dashboard panel

--- pnaa/src/pages/dashboard/Dashboard.jsx
+++ pnaa/src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,33 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getChapterData } from "../../backend/FirestoreCalls";
-import { Button } from "@mui/material";
+import {
+  getChapterData,
+  getRegions,
+  getChapterRegionData,
+} from "../../backend/FirestoreCalls";
+import {
+  Button,
+  Select,
+  Autocomplete,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  TextField,
+  Box,
+  Chip,
+  Paper,
+  Checkbox,
+  FormControlLabel,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+} from "@mui/material";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import SignOutButton from "../../components/SignOutButton/SignOutButton";
+import CloseIcon from "@mui/icons-material/Close";
 import styles from "./Dashboard.module.css";
 import DateSelection from "./DateSelection/DateSelection";
 import dayjs from "dayjs";
@@ -12,11 +36,15 @@ const Dashboard = () => {
   const [chapterData, setChapterData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [national, setNational] = useState(true);
-  const [selectedRow, setSelectedRow] = useState();
   const [openDateSelection, setOpenDateSelection] = useState(false);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [regions, setRegions] = useState([]);
+  const [selectedRegions, setSelectedRegions] = useState([]);
+  const [allChapters, setAllChapters] = useState([]);
+  const [filteredChapters, setFilteredChapters] = useState([]);
+  const [selectedChapters, setSelectedChapters] = useState([]);
+  const [selectAll, setSelectAll] = useState(false);
   const navigate = useNavigate();
 
   //Fetches all member data within chapter
@@ -32,6 +60,45 @@ const Dashboard = () => {
         setLoading(false);
       });
   }, []);
+
+  useEffect(() => {
+    getRegions()
+      .then((region) => {
+        setRegions(region);
+      })
+      .catch((error) => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    getChapterRegionData()
+      .then((chapters) => {
+        setAllChapters(chapters);
+        setFilteredChapters(chapters);
+      })
+      .catch((error) => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const options = Object.entries(filteredChapters).flatMap(([group, items]) =>
+    items.map((label) => ({ group, label }))
+  );
+
+  const handleSelectAll = () => {
+    setSelectAll((prev) => {
+      if (!prev) setSelectedChapters([...options]);
+      else setSelectedChapters([]);
+      return !prev;
+    });
+  };
 
   return (
     <div>
@@ -71,9 +138,169 @@ const Dashboard = () => {
                 <div>SERVICE HOURS</div>
                 <div>####</div>
               </div>
-              <div className={styles.selectionContainers}>
-                <div className={styles.locationContainer}> </div>
-                <div className={styles.locationContainer}> </div>
+              <div className={styles.selectionContainer}>
+                <div className={styles.selectionHeader}>
+                  <h2>Chapters</h2>
+                  <FormControl
+                    sx={{
+                      width: "50%",
+                    }}>
+                    <InputLabel size="small" htmlFor="region-label">
+                      Filter Region
+                    </InputLabel>
+                    <Select
+                      inputProps={{
+                        id: "region-label",
+                      }}
+                      label="Filter Region"
+                      value={selectedRegions}
+                      multiple
+                      size="small"
+                      onChange={(event) => {
+                        setSelectedRegions(event.target.value);
+                        if (event.target.value.length === 0) {
+                          setFilteredChapters(allChapters);
+                        } else {
+                          const filteredRegions = Object.keys(allChapters)
+                            .filter((key) => event.target.value.includes(key))
+                            .reduce((acc, key) => {
+                              acc[key] = allChapters[key];
+                              return acc;
+                            }, {});
+                          setFilteredChapters(filteredRegions);
+                        }
+                      }}
+                      renderValue={(selected) => (
+                        <Box
+                          sx={{
+                            display: "flex",
+                            flexWrap: "wrap",
+                            gap: 0.5,
+                            height: "35px",
+                            maxHeight: "50px",
+                            overflow: "auto",
+                          }}>
+                          {selected.map((value) => (
+                            <Chip key={value} label={value} />
+                          ))}
+                        </Box>
+                      )}
+                      sx={{
+                        "& .MuiSelect-select": {
+                          paddingRight: 4,
+                          paddingLeft: 2,
+                          paddingTop: 1,
+                          paddingBottom: 1,
+                          textAlign: "center",
+                        },
+                      }}
+                      className={styles.select}>
+                      {regions.map((region) => (
+                        <MenuItem key={region} value={region}>
+                          {region}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </div>
+                <Autocomplete
+                  multiple
+                  disableClearable
+                  filterSelectedOptions
+                  isOptionEqualToValue={(option, value) =>
+                    option.label === value.label
+                  }
+                  options={options}
+                  groupBy={(option) => option.group}
+                  getOptionLabel={(option) => option.label}
+                  value={selectedChapters}
+                  onChange={(event, newValue, reason) => {
+                    if (
+                      reason === "selectOption" &&
+                      newValue.length === options.length
+                    )
+                      setSelectAll(true);
+                    setSelectedChapters(newValue);
+                  }}
+                  renderTags={() => null}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      placeholder="Search Chapters"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  )}
+                  PaperComponent={(paperProps) => {
+                    const { children, ...restPaperProps } = paperProps;
+                    return (
+                      <Paper {...restPaperProps}>
+                        <Box
+                          onMouseDown={(e) => e.preventDefault()} // prevent blur
+                          pl={1.5}
+                          py={0.5}>
+                          <FormControlLabel
+                            onClick={(e) => {
+                              e.preventDefault(); // prevent blur
+                              handleSelectAll();
+                            }}
+                            label="Select all"
+                            control={
+                              <Checkbox
+                                id="select-all-checkbox"
+                                checked={selectAll}
+                              />
+                            }
+                          />
+                        </Box>
+                        <Divider />
+                        {children}
+                      </Paper>
+                    );
+                  }}
+                />
+
+                <div className={styles.selectionHeader}>
+                  <h3>Selected Chapters</h3>
+                  <Button
+                    variant="outlined"
+                    className={styles.clearButton}
+                    onClick={() => {
+                      setSelectedChapters([]);
+                      setSelectAll(false);
+                    }}>
+                    Clear All
+                  </Button>
+                </div>
+                <Box
+                  mt={2}
+                  style={{ height: "200px", overflow: "auto", marginTop: "0" }}>
+                  <List
+                    sx={{
+                      paddingTop: "0",
+                    }}>
+                    {selectedChapters.map((chapter) => (
+                      <ListItem key={chapter.label}>
+                        <ListItemText primary={chapter.label} />
+                        <ListItemSecondaryAction>
+                          <IconButton
+                            edge="end"
+                            aria-label="delete"
+                            onClick={() => {
+                              const updatedChapters = selectedChapters.filter(
+                                (item) => item.label !== chapter.label
+                              );
+                              setSelectedChapters(updatedChapters);
+                              setSelectAll(false);
+                            }}>
+                            <CloseIcon />
+                          </IconButton>
+                        </ListItemSecondaryAction>
+                      </ListItem>
+                    ))}
+                  </List>
+                </Box>
               </div>
             </div>
           </div>
